84f3d348c28c36c0144c5d9bee8cee9b4d05dd44
MJDMiner <mark.jayson.aguda@gmail.com>
2023-08-21 15:19:30 +0800

    Added a Rest API called hello-world-bean

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -15,4 +15,8 @@ public class HelloWorldController {
         return "Hello World";
     }
 
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean hellowWordBean(){
+        return new HelloWorldBean("Hello World");
+    }
 }
