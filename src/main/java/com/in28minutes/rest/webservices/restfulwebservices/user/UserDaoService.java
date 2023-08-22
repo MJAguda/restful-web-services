9253a8349d2a8304a793ab6e854d5acce59afc12
MJDMiner <mark.jayson.aguda@gmail.com>
2023-08-22 23:40:11 +0800

    Added retrieveAllUsers and retrieveUser method

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -25,5 +25,16 @@ public class UserDaoService {
     }
     
     // public User save(User user)
-    // public User findOne(int id)
+    public User findOne(int id){
+        
+        // Predicate<? super User> predicate = user -> user.getId().equals(id);
+        // return users.stream().filter(predicate).findFirst.get();
+
+        for (User user : users) {
+            if (user.getId() == id) {
+                return user;
+            }
+        }
+        return null; // Return null if no user with the given ID is found
+    }
 }
